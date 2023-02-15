5a8c2985a549305edc58c82d480cb710f4b233e4
Farouq Siyar <siyar9989@gmail.com>
2023-02-15 14:08:57 +0100

    initialize project

---
+++ src/main/java/com/codecool/TaskTiger/TaskTigerApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.TaskTiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTigerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTigerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/TaskTiger/TaskTigerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.TaskTiger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTigerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
