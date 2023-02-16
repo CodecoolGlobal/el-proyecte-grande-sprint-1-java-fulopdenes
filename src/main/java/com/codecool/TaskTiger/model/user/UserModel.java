c69ddc027f28f54b8ce236d15b4537e2cb88e557
fulopdenes <denes.fulopp@gmail.com>
2023-02-16 09:46:44 +0100

    optimize userModel

--- src/main/java/com/codecool/TaskTiger/model/user/UserModel.java
+++ src/main/java/com/codecool/TaskTiger/model/user/UserModel.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
-public abstract class UserModel {
+public class UserModel {
     private final int id;
     private final String username;
 
@@ -14,22 +14,23 @@ public abstract class UserModel {
     private String lastName;
     private LocalDate dob;
     private Gender gender;
+    private String introduction;
 
     private String email;
     private String phoneNumber;
 
     LocalDateTime registrationDate;
 
-    public UserModel(int id, String username, String firstName, String lastName, LocalDate dob, Gender gender, String email, String phoneNumber, LocalDateTime registrationDate) {
+    public UserModel(int id, String username, String firstName, String lastName, LocalDate dob, Gender gender, String introduction, String email, String phoneNumber, LocalDateTime registrationDate) {
         this.id = id;
         this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
         this.dob = dob;
         this.gender = gender;
+        this.introduction = introduction;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.registrationDate = registrationDate;
     }
-
 }
