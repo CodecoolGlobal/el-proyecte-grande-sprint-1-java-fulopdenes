d51a8f7cb0dc70c78f395fed2ab0269ecc50e8e4
miz092 <mihalyfi.zoltan@gmail.com>
2023-03-06 10:34:14 +0100

    initialize own project repo

--- src/main/java/com/codecool/TaskTiger/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.TaskTiger.repository;
-
-import com.codecool.TaskTiger.model.user.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- src/main/java/com/codecool/TaskTiger/repository/SkillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.TaskTiger.repository;
-
-import com.codecool.TaskTiger.model.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkillRepository extends JpaRepository<Skill, Long> {
-}
