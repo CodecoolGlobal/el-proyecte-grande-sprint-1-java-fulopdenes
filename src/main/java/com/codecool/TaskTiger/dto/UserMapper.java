9653961974b1e1e6cbad7211969c653ba7eff72a
Farouq Siyar <siyar9989@gmail.com>
2023-03-13 13:57:39 +0100

    clean up code

--- src/main/java/com/codecool/TaskTiger/dto/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.TaskTiger.dto;
-
-import com.codecool.TaskTiger.model.user.User;
-import org.springframework.stereotype.Component;
-
-@Component
-public class UserMapper extends AbstractModelMapper<User, UserDTO>{
-}
