d93556277156992c4dbd7b655c334029440648bc
fulopdenes <denes.fulopp@gmail.com>
2023-03-30 21:59:54 +0200

    add four endpoint for TimeSlotController

--- src/main/java/com/codecool/TaskTiger/controller/TimeSlotController.java
+++ src/main/java/com/codecool/TaskTiger/controller/TimeSlotController.java
@@ -1,11 +1,13 @@
 package com.codecool.TaskTiger.controller;
 
+import com.codecool.TaskTiger.dto.TimeSlotStatusDTO;
 import com.codecool.TaskTiger.model.TimeSlot;
 import com.codecool.TaskTiger.service.TimeSlotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("timeslots")
@@ -17,9 +19,27 @@ public class TimeSlotController {
     public TimeSlotController(TimeSlotService timeSlotService) {
         this.timeSlotService = timeSlotService;
     }
-    @GetMapping("/{id}")
-    public List<TimeSlot> getTaskerTimeSlotsById(@PathVariable Long id) {
-        return timeSlotService.getTimeSlotByTaskerId(id);
+
+    @GetMapping("/tasker/{id}")
+    public List<TimeSlot> getTimeSlotByTaskerID(@PathVariable Long id) {
+        return timeSlotService.getTimeSlotByTaskerID(id);
+    }
+
+    @GetMapping("/tasker/slot/{id}")
+    public Optional<TimeSlot> getTimeSlotByID(@PathVariable Long id) {
+        return timeSlotService.getTimeSlotByTimeSlotID(id);
+    }
+
+    @PutMapping("/tasker/slot/")
+    public boolean modifyTimeSlotStatus(@RequestBody TimeSlotStatusDTO timeSlotStatus) {
+        return timeSlotService.modifyTimeSlotStatus(timeSlotStatus);
+    }
+
+    @PutMapping("/tasker/reservation/modify/{reservationId}")
+    public boolean modifyTimeSlotStatusByReservationId(
+            @RequestBody TimeSlotStatusDTO timeSlotStatus,
+            @PathVariable Long reservationId) {
+        return timeSlotService.modifyTimeSlotStatusByReservationId(reservationId, timeSlotStatus);
     }
 
 }
