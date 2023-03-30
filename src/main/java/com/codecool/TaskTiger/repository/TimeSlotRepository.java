65bbdf1748a315b624ecd7c4acfb5b0f2104f083
fulopdenes <denes.fulopp@gmail.com>
2023-03-30 22:00:25 +0200

    add two method for this repository

--- src/main/java/com/codecool/TaskTiger/repository/TimeSlotRepository.java
+++ src/main/java/com/codecool/TaskTiger/repository/TimeSlotRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TimeSlotRepository extends JpaRepository<TimeSlot, Long> {
     List<TimeSlot> findTimeSlotByTaskerId(Long id);
+    Optional<TimeSlot> findTimeSlotById(Long id);
+    List<TimeSlot> findTimeSlotsByReservationId(Long id);
 }
